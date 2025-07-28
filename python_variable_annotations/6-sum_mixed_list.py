b3f3fa0a482254b0849323c13143f149c04b352d
romain <romainleoulzec@gmail.com>
2025-07-28 17:40:22 +0200

    return to the max sum of mxd_lst

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""définie la fonction sum_mixed_list"""
+
+
+def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """renvoie la somme des éléments d'une liste mixte"""
+    return sum(mxd_lst)
