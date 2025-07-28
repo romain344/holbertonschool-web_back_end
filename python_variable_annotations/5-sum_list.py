b113a4945852a2537bf95491537da1853186104a
romain344 <romainleoulzec@gmail.com>
2025-07-28 16:23:19 +0200

    function that returns the sum for float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""dèfinie la fonction sum_list"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """retourne à les élément de unut_list"""
+    return sum(input_list)
