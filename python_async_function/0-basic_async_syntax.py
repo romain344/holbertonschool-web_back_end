1cd7c0045a8b542f7ed615720c17281b7c307326
romain344 <romainleoulzec@gmail.com>
2025-07-29 16:18:44 +0200

    function for genérate one number between zero and ten

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""défine la fonction async wait_random"""
+
+
+import random
+import asyncio
+
+async def wait_random(max_delay):
+    """choissi un temps dattante entre 0 et 10"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
