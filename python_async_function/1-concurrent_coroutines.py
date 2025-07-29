396e8a64fe7ac149078ea7a275a1ef9d68b5e90b
romain344 <romainleoulzec@gmail.com>
2025-07-29 16:23:03 +0200

    task1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Définit la coroutine async wait_random"""
+
+import random
+import asyncio
+
+async def wait_random(max_delay=10):
+    """retourne à delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
