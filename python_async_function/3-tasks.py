5a338b6fc32bbd673b19ef217352a412226dcd8f
romain344 <romainleoulzec@gmail.com>
2025-07-30 16:34:17 +0200

    use a function for execute another function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 wait_random = __import__ ('0-basic_async_syntax').wait_random
 
 
-async def task_wait_random(max_delay: int) -> asyncio.tasks:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """execute la fonction wait_random"""
     return asyncio.create_task(wait_random(max_delay))
