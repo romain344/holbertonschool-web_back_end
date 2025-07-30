d8e0f80b8646e2f5e22b4e7912c77880051355f9
romain344 <romainleoulzec@gmail.com>
2025-07-30 10:23:38 +0200

    return to the max sum of mxd_ist

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """dèfinie la fonction sum_list"""
 
 
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """retourne à les élément de unut_list"""
