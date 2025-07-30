87f946bcc686ff8b753a3e1492dfb312499fc0fc
romain344 <romainleoulzec@gmail.com>
2025-07-30 15:00:32 +0200

    measure the time medium for the function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,7 +4,7 @@
 
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_randomn
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
