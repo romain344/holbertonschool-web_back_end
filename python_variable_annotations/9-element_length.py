0363546605a444ccea64b1b43fd200daec9a9b5a
romain344 <romainleoulzec@gmail.com>
2025-07-30 10:27:56 +0200

    add type annotation to element_lengyh function for iterable sequance and tuple return type

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
-""""""
+#!/usr/bin/env python3
+"""définie la fonction elment_length"""
 
 
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return à i """
     return [(i, len(i)) for i in lst]
