19b1d127ba52b4f6fa5b44a2a7536934899785b0
romain <romainleoulzec@gmail.com>
2025-07-30 21:16:49 +0200

    generate a number, one between and ten to generate ten values in one second

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,6 +9,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """Génère 10 valeurs aléatoires entre 0 et 10 avec un délai d'1 seconde entre chaque"""
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+for _ in range(10):
+    await asyncio.sleep(1)
+    yield random.uniform(0, 10)
