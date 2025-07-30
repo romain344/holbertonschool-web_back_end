625b4baaa604c670011b0a3cf5b251a45a6b5bec
romain344 <romainleoulzec@gmail.com>
2025-07-30 13:18:59 +0200

    task1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 """Définit la coroutine async wait_random"""
 
-import random
-import asyncio
 
-async def wait_random(max_delay=10):
-    """retourne à delay"""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Exécute wait_random"""
+    delays: List[float] = []
+
+    async def get_and_store_delay():
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    tasks = [asyncio.create_task(get_and_store_delay()) for _ in range(n)]
+    await asyncio.gather(*tasks)
+
+    sorted_delays: List[float] = []
+    for d in delays:
+        for i in range(len(sorted_delays)):
+            if d < sorted_delays[i]:
+                sorted_delays.insert(i, d)
+                break
+        else:
+            sorted_delays.append(d)
+
+    return sorted_delays
