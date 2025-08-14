88b00e32f41bc00622fdf534cfbb5480377487a9
romain <romainleoulzec@gmail.com>
2025-08-14 15:31:07 +0200

    return to data page

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,8 +4,8 @@
 from typing import Tuple
 
 
-def index_page(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """retourne à start en end pour la page de données"""
-    start_index: int = (page - 1) * page_size
-    end_index: int = start_index + page_size
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
     return start_index, end_index
