02d3507baaf192a6ae779cd6b05bf0778b7325b4
romain <romainleoulzec@gmail.com>
2025-08-14 15:27:54 +0200

    return to data page

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,8 +2,11 @@
 """execute la fonction index_page"""
 
 
-def index_page(page, page_size):
-    """retorune à start en end pour la page de données"""
-    start =  (page - 1) * page_size
-    end = start * page_size
-    return (start, end)
+from typing import Tuple
+
+
+def index_page(page: int, page_size: int) -> Tuple[int, int]:
+    """retourne à start en end pour la page de données"""
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    return start_index, end_index
