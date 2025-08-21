86aee1957ea40320cdf160c9964e6a2b740b8386
romain344 <romainleoulzec@gmail.com>
2025-08-21 16:32:47 +0200

    print a error when a value is missing

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+    constructor(code, name) {
+        if (typeof code  !== 'string') {
+            throw new TypeError('code msut be a string');
+        }
+        if (typeof name !== 'string') {
+            throw new TypeError('name must be a string');
+        }
+        this._code = code;
+        this._name = name;
+    }
+    
+    get code() {
+        return this._code;
+    }
+    set code(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('code msut be a string')
+        }
+        this._code = value;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('name must be a string');
+        }
+        this._name = value;
+    }
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
