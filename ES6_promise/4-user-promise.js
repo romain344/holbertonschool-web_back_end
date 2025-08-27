a274779ff045c8f8236bb7a1aa40cf71e36f78dd
romain344 <romainleoulzec@gmail.com>
2025-08-27 13:58:01 +0200

    function thats check and return to the promises

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-    return {
-        firstName: firstName,
-        lastName: lastName
-    };
+    return Promise.resolve({ firstName, lastName });
 }
