13fe7a32defe4f4247c379ad4d493160fb7b3e60
romain344 <romainleoulzec@gmail.com>
2025-08-27 11:35:59 +0200

    requirment

---
+++ ES6_promis/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
