8dcb68cc1e4db9e3f25601ec137e2bd0091effc2
romain344 <romainleoulzec@gmail.com>
2025-08-27 13:35:02 +0200

    function thats check and return to the promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,5 @@
 import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
     Promise.all([uploadPhoto(), createUser()])
         .then(([photo, user]) => {
@@ -7,4 +8,3 @@ export default function handleProfileSignup() {
         .catch(() => {
             console.log('Signup system offline');
         });
-}
