d7c606968ce926d354cbc860f098785a9479c34e
romain344 <romainleoulzec@gmail.com>
2025-08-28 12:46:10 +0200

    task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        uploadPhoto(fileName),
-        signUpUser(firstName, lastName)
-    ]).then((results) =>
-        results.map((result) => ({
-            status: result.status,
-            value: result.value || result.reason,
-        }))
-    );
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) =>
+    results.map((r) => ({
+      status: r.status,
+      value: r.value || r.reason,
+    }))
+  );
 }
