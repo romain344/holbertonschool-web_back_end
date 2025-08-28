a220caec4e7c3e62bca9c94926dcc815fdb4d772
romain344 <romainleoulzec@gmail.com>
2025-08-28 11:14:09 +0200

    function take 2 argument and 1 argument print one faill as to the number equal to zero

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,13 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    return new Promise((resolve, reject) => {
-        try {
-            if (denominator === 0) {
-                throw new Error('cannot divide by 0');
-            }
-            const result = numerator / denominator;
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        }
-    });
+    if (denominator === 0) {
+        throw new Error('cannot divide by 0');
+    }
+    return numerator / denominator;
 }
