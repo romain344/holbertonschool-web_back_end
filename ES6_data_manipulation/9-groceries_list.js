7495d4548a1a30ee9724535c9f4b8ff7a3853311
romain344 <romainleoulzec@gmail.com>
2025-08-28 13:27:26 +0200

    function thats to  items

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
-    return [
-        'Apples', 10,
-        'Tomatoes', 10,
-        'Pasta', 1,
-        'Rice', 1,
-        'Banana', 5
-    ]
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
 }
