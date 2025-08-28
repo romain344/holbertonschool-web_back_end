3ef6a3592c2d32ebf1c6707a2da2ac06faa58c8f
romain344 <romainleoulzec@gmail.com>
2025-08-28 13:15:23 +0200

    made between a whole between to valleur

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,11 @@
-function createInt8TypedArray() {
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range')
-    }
-    const buffer = new ArrayBuffer(length);
-    const view = new DataView(buffer);
-    view.setInt8(position, value);
-    return view;
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+  return view;
 }
